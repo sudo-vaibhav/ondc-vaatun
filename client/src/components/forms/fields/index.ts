bddd1dca7a6cc6c669b2ce0ac97e6028a2fa40b4
sudo-vaibhav <53619134+sudo-vaibhav@users.noreply.github.com>
2026-02-03 17:29:33 +0530

    feat(02-03): add PEDSelector component and barrel export
    - PEDSelector: checkbox list of 7 PED condition categories
    - Collapsible 'Other' text field animates when checkbox is toggled
    - PED_CONDITIONS constant exported for form schema use
    - Barrel export provides clean import path for all field components

---
+++ client/src/components/forms/fields/index.ts
@@ -0,0 +1,10 @@
+export { PANInput, type PANInputProps } from "./PANInput";
+export { PhoneInput, type PhoneInputProps } from "./PhoneInput";
+export { DateInput, type DateInputProps } from "./DateInput";
+export {
+  PEDSelector,
+  type PEDSelectorProps,
+  type PEDConditions,
+  type PEDConditionId,
+  PED_CONDITIONS,
+} from "./PEDSelector";
